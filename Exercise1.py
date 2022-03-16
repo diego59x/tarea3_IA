34a340f4d953df80f034cdd9ca307ed9ed9c51cf
diegoalvarez17 <diegoalvarez1237@hotmail.com>
2022-03-15 22:24:47 -0600

    Added graphics
    Co-Authored-By: wsaldana <53311124+wsaldana@users.noreply.github.com>

--- Exercise1.py
+++ Exercise1.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-
+from matplotlib import pyplot as plt
+import math
 # FIRST ========================================
 def firstEquation(X):
     x, y = X
@@ -10,7 +10,6 @@ def firstEquationDeri(X):
     x, y = X
     return np.array(4*x**3 - 4*y, -4*x + 4*y**3 + 0.5)
 
-
 # SECOND =======================================
 def secondEquation(X):
     x1, x2 = X
@@ -19,8 +18,6 @@ def secondEquation(X):
 def secondEquationDeri(X):
     x1, x2 = X
     return np.array(2*(200*x1**3 - 200*x1*x2 + x1 - 1 ), 200*(x2-x1**2))
-    # 2 (-1 + x + 200 x^3 - 200 x y)
-
 
 # THIRD =======================================
 def thirdEquation(x):
@@ -34,8 +31,7 @@ def thirdEquation(x):
 
 def thirdEquationDeri(X):
     gradient = []
-    # print(X)
-    # print(secondEquationDeri([X[0], X[1]]))
+
     gradient.append(secondEquationDeri([X[0], X[1]]))
     for i in range(1, 9):
         x, y, z = X[i-1], X[i], X[i+1]
@@ -45,40 +41,60 @@ def thirdEquationDeri(X):
 
     return np.array(gradient)
 
-# def f_rosenbrock(xy):
-#     x, y = xy
-#     return 100 * (y - x**2)**2 + (1 - x)**2
+def stepDepeest(x0, f, df, alpha= 0.1, maxI=500, tolx=1e-4, tolf= 1e-4, tolg=1e-4, metric = 2):
+    iters = 0
+    fin = 0
+    convergence = 0
+    x = x0.copy()
 
-# def df_rosenbrock(xy):
-#     x, y = xy
-#     return np.array([-400*x*(y-x**2)-2*(1-x), 200*(y-x**2)])
+    approx = []
+    values = []
+    grads = []
+    errs = []
+    approx.append(x)
+    values.append(f(x0))
+    grads.append(df(x0))
+    errs.append(np.linalg.norm(x0, 1))
 
-def gradient_descent(f, df, x0, tol=.1, alpha=1.0, ratio=.8, c=.01):
-    x_k, num_steps, step_size = x0, 0, alpha
-    while True:
-        g_k = df(x_k)
+    while(fin == 0 and iters < maxI):
+        oldx = x
+        oldalpha = alpha
+        gr = df(oldx)
+        x = oldx - alpha * gr
+        fx = f(oldx)
+        dfx = df(x)
 
-        if np.abs(g_k).max() < tol:
-            break
+        iters += 1
+        approx.append(x)
+        values.append(fx)
+        grads.append(dfx)
 
-        num_steps += 1
+        error = np.linalg.norm(oldx-x, metric)
+        if error < tolx:
+            fin = 1
+            convergence = 1
+        if (math.isnan(error) != False):
+            errs.append(error)
 
-        fx, cg = f(x_k), - c * (g_k**2).sum()
-        while f(x_k - step_size * g_k) > fx + step_size * cg:
-            step_size *= ratio
-
-        x_k -= step_size * g_k
-
-    return x_k, g_k, num_steps
+    print("=======================================")
+    print("Best: ", values[-1])
+    print("Errors: ", errs)
+    print("Point vals: ", approx)
+    print("Func vals: ", values)
+    print("Iters: ", iters)
+    print("Convergent: ", convergence)
+    print("=======================================")
+    return [values, errs, approx, values, iters, convergence]
 
 if __name__ == '__main__':
-    x, g, n = gradient_descent(
-        thirdEquation, thirdEquationDeri, np.array([0., 0., 0., 0.,0.,0., 0., 0., 0.,0.])
-    )
-    print("The number of steps is: ", n)
-    print("The final step is:", x)
-    print("The gradient is: ", g)
+    errs1 = stepDepeest(np.array([0., 1.]), firstEquation, firstEquationDeri)[1]
+    errs2 = stepDepeest(np.array([1., 3.]), secondEquation, secondEquationDeri)[1]
+    errs3 = stepDepeest(np.array([1., 0., 0., 1., 1., 3., 0., 1., 0., 0.]), thirdEquation, thirdEquationDeri)[1]
 
+    for err in (errs1, errs2, errs3):
+        plt.figure()
+        plt.plot(err[1:len(err)])
+        plt.xlabel('Step')
+        plt.ylabel('Error')
+        plt.show()
 
-# if __name__ == "__main__":
-#     print(stepDepeest(np.array([0.0, 0.0]), quad, dquad))
